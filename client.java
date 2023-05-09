9de2f0abcd5a5c9a9af7b40315babb129a05783d
lkasdj02 <leonardo.basso02@gmail.com>
2023-05-09 11:53:41 +0200

    first commit

---
+++ client.java
@@ -0,0 +1,45 @@
+import java.net.*;
+import java.io.*;
+
+class client {
+	public static void main(String[] args) {
+
+
+		// setup iniziale
+		String inputline = ""; 
+		int portNumber = 1234;
+
+		try {
+
+			// creating socket.
+			Socket socket =  new Socket("localhost", portNumber);
+			PrintWriter    outServer	= new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader inServer		= new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			BufferedReader inKeyboard = null;
+			do {
+
+				inKeyboard = new BufferedReader(new InputStreamReader(System.in));
+
+				// leggere input da tastiera (unica parte bloccante del codice).
+				inputline  = inKeyboard.readLine();
+				System.out.println("invio: " + inputline);
+				outServer.println(inputline); // parte nella quale mando qualcosa al server
+
+				System.out.println("ricevuto: " + inServer.readLine());
+
+			} while (!inputline.equals("BYE"));
+
+			// rilascio delle risorse.
+			outServer.close();
+			inServer.close();
+			inKeyboard.close();
+			socket.close();
+
+
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+
---
+++ server.java
@@ -0,0 +1,41 @@
+import java.net.*;
+import java.io.*;
+
+public class server {
+	public static void main(String[] args) throws IOException {
+		while(true) {
+			// setup iniziale	
+			String inputline = "";
+			final String msg = "ciao client! ";
+			final int portNumber = 1234;
+			ServerSocket serverSocket = new ServerSocket(portNumber);
+
+			Socket soc	= serverSocket.accept(); // accept ritorna solo un socket.
+			System.out.println("connessione accettatata...");
+
+			// setup degli I/O di questo endpoint
+			PrintWriter  outServer		= new PrintWriter(soc.getOutputStream(), true); 
+			BufferedReader inServer		= new BufferedReader(new InputStreamReader(soc.getInputStream()));
+
+			while(!inputline.equals("BYE")) { // alla prima iterazione questa condizione == false
+				try {
+
+					inputline = inServer.readLine(); // parte bloccante del codice
+																					 // una buona idea sarebbe mettere questa parte di computazione in un thread separato
+																					 // per non bloccare la principale linea di esecuzione (=> main thread).
+					System.out.println("ricevuto: " + inputline);
+					// invio risposta da parte del server 
+					outServer.println(msg + "ho ricevuto: " + inputline);	
+
+				} catch(IOException e) {
+					e.printStackTrace();
+				}
+			}
+			// rilascio delle risorse
+			outServer.close();
+			inServer.close();
+			serverSocket.close();
+			soc.close();
+		}
+	}
+}
